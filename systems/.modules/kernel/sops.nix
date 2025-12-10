871e377ec96d3b0bcfaf6fb7f5a09cdd3a1a3c14
mih4n <losevmisha0@gmail.com>
2025-12-10 22:51:57 +0300

    changed sops

--- systems/.modules/kernel/sops.nix
+++ systems/.modules/kernel/sops.nix
@@ -2,7 +2,7 @@
     sops.defaultSopsFile = ../../../secrets/secrets.yaml;
     sops.defaultSopsFormat = "yaml";
 
-    sops.age.keyFile = "/home/bytekeeper/.config/sops/age/keys.txt";
+    sops.age.keyFile = "/etc/ssh/ssh_host_ed25519_key";
 
     sops.secrets."bytes" = {};
     sops.secrets."nextcloud/adminpass" = {};    
