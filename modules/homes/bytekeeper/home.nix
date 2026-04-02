df2702a3a8bdeb6dff86ab01e7b5c45f0fa5f81e
mih4n <losevmisha0@gmail.com>
2026-04-03 01:05:27 +0300

    fix

--- modules/homes/bytekeeper/home.nix
+++ modules/homes/bytekeeper/home.nix
@@ -13,10 +13,6 @@
             self.homeModules.vscodeServer
         ];
 
-        bytes.home = {
-            vscode.enable = true;
-        };
-
         home = {
             username = "bytekeeper";
             stateVersion = "25.05";
